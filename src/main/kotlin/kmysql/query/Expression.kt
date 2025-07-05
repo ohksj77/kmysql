77a03bdbe3fbac818fc79d14a99353765d9a88cd
김승진 <ohksj77@gmail.com>
2025-07-06 02:18:15 +0900

    feat: sql query

---
+++ src/main/kotlin/kmysql/query/Expression.kt
@@ -0,0 +1,58 @@
+package kmysql.query
+
+import kmysql.record.Schema
+
+class Expression {
+    private var value: Constant? = null
+    private var fieldName: String? = ""
+
+    constructor(value: Constant) {
+        this.value = value
+    }
+
+    constructor(fieldName: String) {
+        this.fieldName = fieldName
+    }
+
+    fun isFieldName(): Boolean {
+        return fieldName != null
+    }
+
+    fun asConstant(): Constant {
+        return value ?: throw RuntimeException("null error")
+    }
+
+    fun asFieldName(): String {
+        return fieldName ?: throw RuntimeException("null error")
+    }
+
+    fun evaluate(scan: Scan): Constant {
+        return if (value != null) {
+            value!!
+        } else if (fieldName != null) {
+            scan.getVal(fieldName!!)
+        } else {
+            throw RuntimeException("null error")
+        }
+    }
+
+    fun appliesTo(schema: Schema): Boolean {
+        return if (value != null) {
+            true
+        } else if (fieldName != null) {
+            schema.hasField(fieldName!!)
+        } else {
+            throw RuntimeException("null error")
+        }
+    }
+
+    override fun toString(): String {
+        return if (value != null) {
+            value!!.toString()
+        } else if (fieldName != null) {
+            fieldName!!
+        } else {
+            throw RuntimeException("null error")
+        }
+    }
+}
